a7781444f29ea7a009bb0530cda7c53c4067cb1c
Asil <Turkmenasil@hotmail.com>
2025-08-16 12:06:16 +0300

    firebase bağlandı

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,21 +1,32 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { Platform } from "react-native";
+import { getAuth, initializeAuth, type Auth } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const firebaseConfig = { 
   apiKey: "AIzaSyAD26IMDbKuAKB2mrTI-H0m8IJR7pIHuD8",
   authDomain: "kasa-ae2cb.firebaseapp.com",
   projectId: "kasa-ae2cb",
-  storageBucket: "kasa-ae2cb.firebasestorage.app",
+  storageBucket: "kasa-ae2cb.appspot.com",
   messagingSenderId: "510257612644",
   appId: "1:510257612644:web:2e138aef2be7fe5d301c71",
   measurementId: "G-9RWRMTGL50"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+let auth: Auth;
+if (Platform.OS === "web") {
+  auth = getAuth(app);
+} else {
+  // TS kırmızılığını engelleyen dinamik import
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { getReactNativePersistence } = require("firebase/auth/react-native");
+  auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage),
+  });
+}
+
+const db = getFirestore(app);
+export { app, auth, db };
